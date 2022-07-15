b55411676f290e96731e17d831001d4d861555fc
LuvSahu <104768173+LuvSahu@users.noreply.github.com>
2022-07-15 17:57:44 +0530

    Delete EmployeeTable directory

--- EmployeeTable/CreateTable.sql
+++
@@ -1,9 +0,0 @@
-----Create table------
-CREATE TABLE employee_payroll (
-    Id int identity(1,1) PRIMARY KEY,
-	--first 1 = it should be start with first row and second indicate 1 = increment by 1---
-    Name varchar(200),
-    Salary float,
-    StartDate datetime,
-   
-);
