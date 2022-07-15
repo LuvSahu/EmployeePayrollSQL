3aedc35003cede8b0fd7f9d2bf84a5782dff0765
LuvSahu <104768173+LuvSahu@users.noreply.github.com>
2022-07-15 17:56:34 +0530

    Delete InsertData directory

--- InsertData/InsertDataIntoTable.sql
+++
@@ -1,6 +0,0 @@
----------to insert data into the table UC3--------------
-INSERT INTO employee_payroll
-VALUES('Luv',50002,'2010-02-02'),('Atul',50003,'2012-04-01');
-
-INSERT INTO employee_payroll(Name,StartDate)
-VALUES('Deepak','2021-02-02'),('Rohit','2019-07-01');
