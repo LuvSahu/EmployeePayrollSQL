d03dd4cfdafd48605149f0f47c5c884b6fd26402
LuvSahu <104768173+LuvSahu@users.noreply.github.com>
2022-07-15 17:58:23 +0530

    Delete RetriveSalaryAndDateRange directory

--- RetriveSalaryAndDateRange/RetriveTableviaNameandDateRange.sql
+++
@@ -1,7 +0,0 @@
---------UC5 retrive accoring to data and particular employee-----
-SELECT * FROM  employee_payroll where Name='Luv' or Name='Sahu';
-----------to get the d/f colum-------
-SELECT * FROM  employee_payroll where Name='Luv' or Salary=50002;
-
-----------------UC5 retrive according to the start date range-------------
-SELECT * FROM  employee_payroll where StartDate between CAST('2012-04-01' as date) and GETDATE();
