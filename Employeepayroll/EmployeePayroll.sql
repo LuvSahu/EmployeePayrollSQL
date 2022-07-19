602aaf1951ccbbd877bb7e100572ad5202b6b04f
Luv <104768173+LuvSahu@users.noreply.github.com>
2022-07-19 17:59:21 +0530

    Update EmployeePayroll.sql

--- Employeepayroll/EmployeePayroll.sql
+++ Employeepayroll/EmployeePayroll.sql
@@ -78,3 +78,98 @@ select * from employee_payroll;
 ALTER TABLE employee_payroll
 ADD Address varchar(200) default 'Dehli';
 
+-------------UC9 Extend the employee table----------------
+ALTER TABLE employee_payroll ADD BasicSalary float,Deductions float,TaxablePay float,IncomeTax float,NetPay float;
+
+--------------UC10 ability to make same employee for the two different department-----------
+SELECT* FROM  employee_payroll;
+UPDATE employee_payroll set Department='IT', Address ='Gurgaon',BasicSalary=556786, Deductions=200,TaxablePay=3000 where Name='Luv';
+UPDATE employee_payroll set Department='Software', Address ='Bengaluru',BasicSalary=456796, Deductions=300,TaxablePay=2000 where Name='Deepak';
+UPDATE employee_payroll set Department='UPSC',Address ='Dehli',BasicSalary=356786, Deductions=270,TaxablePay=3000 where Name='Rohit';
+UPDATE employee_payroll set Department='Devops',Address ='Noida',BasicSalary=459786, Deductions=280,TaxablePay=3000 where Name='Atul';
+UPDATE employee_payroll set Department='Sales', Address ='Gurgaon',BasicSalary=456786, Deductions=200,TaxablePay=3000 where Name='Shubham';
+UPDATE employee_payroll set Department='Sales', Address ='Bhiwadi',BasicSalary=456786, Deductions=200,TaxablePay=3000 where Name='Suraj';
+-----INSERTING ONE MORE ROW WITH SAME EMPLOYEE DETAILS-----------------
+
+INSERT INTO employee_payroll(Name,Salary,StartDate,Gender,PhoneNumber,Department,Address,BasicSalary,Deductions,TaxablePay,IncomeTax,NetPay)
+VALUES('Sandeep',50002,'2008-02-02','M',5436787654,'Marketing','Mysore',4567860,200,3000,8765,600000);
+
+-------UC11-creating the new tables for many to many relation-----------------
+CREATE TABLE EmployeeDeparmentt(
+DeptId int identity(1,1) PRIMARY KEY,
+
+EmployeeID int FOREIGN KEY REFERENCES employee_payroll(Id));
+SELECT* FROM  EmployeeDeparmentt;
+INSERT INTO EmployeeDeparmentt(EmployeeID) values(1);
+INSERT INTO EmployeeDeparmentt(EmployeeID) values(2);
+INSERT INTO EmployeeDeparmentt(EmployeeID) values(3);
+INSERT INTO EmployeeDeparmentt(EmployeeID) values(4);
+INSERT INTO EmployeeDeparmentt(EmployeeID) values(5);
+INSERT INTO EmployeeDeparmentt(EmployeeID) values(6);
+
+create table payroll
+(
+	Payroll_ID int not null primary key,
+	Basic_Pay money not null,
+	Deduction float not null,
+	Taxable real, 
+	Income_Tax float,
+	Netpay real, 
+)
+
+insert into payroll values
+	(101,800000,2200.20,6000,50000,25000),
+	(102,900000,6600.20,4000,70000,55000),
+	(103,700000,7700.20,5000,70000,55000),
+	(104,600000,9900.10,3000,80000,56000)
+
+select *from payroll
+
+create table Company
+(
+	Company_ID int primary key,
+	ComapnyName varchar(20)
+)
+
+insert into Company values
+	(801,'Atotech'),
+	(802,'Google'),
+	(803,'IBM'),
+	(804,'MINDTREE')
+
+select * from Company
+
+create table Employee
+(
+	Employee_ID int not null primary key,
+	Name varchar(20) not null,
+	Gender varchar(1) not null,
+	Basic_pay money not null,
+	StartDate date not null,
+	PhoneNumber varchar(10),
+	Addres varchar(60),
+	CompanyEmployeeID int FOREIGN KEY REFERENCES Company(Company_ID),
+	PayrolID int FOREIGN KEY  REFERENCES Payroll(Payroll_ID)
+)
+
+insert into Employee values
+	(001,'Luv','M',700000,'2018-07-11','8502012867','Gurguram',801,101),
+	(002,'Rohit','M',800000,'2021-09-05','8502018567','Alwar',802,102),
+	(003,'Priyanka','F',600000,'2019-08-09','7894561238','Ajmer',803,103),
+	(004,'Sachin','M',900000,'2018-07-05','9632587417','Dehli',804,104);
+
+select * from Employee
+
+-----------UC12-checking with UC-4,5,7----------
+SELECT* FROM  employee_payroll;
+
+
+
+SELECT SUM(Salary) as TotalSalary from employee_payroll;
+SELECT avg(Salary) as AvergeSalary from employee_payroll;
+SELECT  MIN(Salary) as MINIMUMSalary from employee_payroll;
+SELECT MAX(Salary) as MaximumSalary from employee_payroll;
+SELECT COUNT(Salary) from employee_payroll;
+
+
+
